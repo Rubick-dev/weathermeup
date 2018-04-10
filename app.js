c5cde19c88a5bd754c89e408e5bf4e37d3e3ed61
Heath Burton <rubickdev@gmail.com>
2018-04-10 21:13:27 +1000

    Final product, first release ready for upload.

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-const API_KEY = "";
+$( document ).ready(function(){
+  var latitude, longitude;
+  var fTemp = 0;
+  var temp = "Curent Temperature";
+  const clickToggleButton = document.getElementById("tempTypeToggle");
 
-function getWeather() { 
-  $.ajax({
-    url: 'https://fcc-weather-api.glitch.me/',
-    type: 'GET',
-    data: {},
-    dataType:'json',
-    success: function(data) { 
+  // Gather the latitude and longtitude of users current location or provide error message
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = "lat=" + position.coords.latitude;
+      longitude = "&lon=" + position.coords.longitude;
+      console.log(latitude + " " + longitude);
+      getWeather(latitude, longitude);
+    });
+  } else {
+      Alert('Geo Location is not enabled or setup correctly on this device');
+  }
 
-    },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("", API_Key);}
+  var customURL = "https://fcc-weather-api.glitch.me/api/current?"
+  function getWeather(latitude, longitude) { 
+    let location = "City name - country name";
+    let status = "Current Temp";
+    let icon = "to be image URL";
+    $.ajax({
+      url: customURL + latitude + longitude,
+      success: function(data) { 
+        location = data.name + " - " + data.sys.country;
+        temp = data.main.temp;
+        icon = data.weather[0].icon;
+        console.log(icon);
+        document.getElementById("city").innerHTML = location;
+        document.getElementById("temp").innerHTML = temp;
+        document.getElementById("weatherIcon").src = icon;
+        document.getElementById("weatherIcon").removeAttribute("height");
+        document.getElementById("weatherIcon").removeAttribute("width");
+        fTemp = Math.round((temp * (9/5)) + 32);
+      },
+   });
+  }
+  
+  // Event Listener for toggling the Temp between Celsius and Fahrenheit
+  clickToggleButton.addEventListener('click', function(){
+    if(clickToggleButton.classList.contains("celsius")) {
+      clickToggleButton.classList.add("fahrenheit");
+      clickToggleButton.classList.remove("celsius");
+      document.getElementById("temp").innerHTML = fTemp;
+      document.getElementById("tempType").innerHTML = " °F";
+    } else {
+      clickToggleButton.classList.remove("fahrenheit");
+      clickToggleButton.classList.add("celsius");
+      document.getElementById("temp").innerHTML = temp;
+      document.getElementById("tempType").innerHTML = " °C";
+    }
   });
-}
+
+}); // End of Ready document
